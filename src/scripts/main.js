b697637156e25e515cd62543b8e6189a13010fac
Julia <jv@bananazbrain.ru>
2022-10-21 12:09:16 +0300

    index animated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
     // set css variables
     setSizes();
     window.addEventListener('resize', setSizes);
+
+    // animation
+    let anBlocks = document.querySelectorAll('.an');
+    function animatedBlocks() {
+        let Y = window.scrollY;
+        let visibleHeight = window.innerHeight - 100;
+        anBlocks.forEach((block) => {
+            if (!block.classList.contains('--loaded')) {
+                let timeout = block.getAttribute('data-timeout');
+                if (timeout) {
+                    block.style.transitionDelay = timeout;
+                }
+                if (block.getBoundingClientRect().top < visibleHeight) {
+                    block.classList.add('--loaded');
+                }
+            }
+        });
+    }
+    setTimeout(() => {
+        animatedBlocks();
+        document.addEventListener('scroll', () => {
+            animatedBlocks();
+        });
+    }, 500);
 });
 
 function initDefaultClasses() {
